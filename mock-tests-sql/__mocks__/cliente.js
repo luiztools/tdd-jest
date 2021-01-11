e01b1789c4109138c06de636ad212bb4c2a7565c
luiztools <luizfduartejr@gmail.com>
2021-01-11 14:01:14 -0300

    sequelize-mock fixes

--- mock-tests-sql/__mocks__/cliente.js
+++ mock-tests-sql/__mocks__/cliente.js
@@ -6,7 +6,17 @@ const ClienteMock = dbMock.define('cliente', {
     nome: 'Luiz',
     idade: 32,
     uf: 'RS'
+}, {
+    instanceMethods: {
+        save: function () {
+            if (this.nome.length > 150) throw new Error('MAX LENGTH ERROR');
+            this.id = 2;
+            return this;
+        }
+    }
 });
+
+ClienteMock.count = () => { return 10; }
 
 ClienteMock.$queryInterface.$useHandler((query, queryOptions, done) => {
     if (query === 'findAll') {
